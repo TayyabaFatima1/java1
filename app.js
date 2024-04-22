cdbe333ccfd7f7b67d8d544eefff190dee8e8431
tayyabaatima1 <yasmeenrahim.huda@gmail.com>
2024-04-22 08:56:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Hey! Glad to see you.");
+
+alert("Error! Please enter a valid password.");
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+alert("Welcome to JS Land.");
+
+alert("Happy coding! \nPrevent this page from creating additional dialogs ");
